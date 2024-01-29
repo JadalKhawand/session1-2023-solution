69f2430941aa477368ed25f1f06e5a3b1470c774
Jad alKhawand <jkh42005@gmail.com>
2024-01-29 06:40:53 +0200

    solution for the 2023 session 1 in I2204 course

--- main.c
+++ main.c
@@ -24,6 +24,11 @@ typedef struct Store
     NodeP *products; // pointer to the first product
 } Store;
 
+typedef struct Node{
+    int data;
+    Node *next;
+}Node;
+
 int getQuantity(Store *s, int id)
 {
     NodeP* current = s->products;
@@ -67,6 +72,62 @@ void SaveProducts(Store *s){
     if(fclose(file))
         printf("Error saving the file.");
 }
+
+void updateProductPrice(int id, float price){
+    FILE *file = fopen("products.dat","rb+");
+    if(!file)
+    {
+        printf("Failed to open file for reading and writing.");
+        return;
+    }
+    Product product;
+    while(fread(&product, sizeof(Product),1,file)){
+        if(product.id = id){
+            product.price = price;
+            fseek(file,-sizeof(Product),SEEK_CUR);
+            fwrite(&product, sizeof(Product),1,file);
+            printf("Price updated");
+            fclose(file);
+            return;
+        }
+    }
+    printf("Product with ID %d not found\n", id);
+    fclose(file);
+}
+
+Node* partition(Node *head, int input) {
+    Node *lessThan = NULL;
+    Node *greaterThanOrEqual = NULL;
+    Node *lessThanEnd = NULL;
+    Node *current = head;
+
+    while (current != NULL) {
+        Node *temp = current->next;
+
+        if (current->value < input) {
+//If lessThan is NULL, we set both lessThan and lessThanEnd to current.
+            if (lessThan == NULL) {
+                lessThan = current;
+                lessThanEnd = current;
+// we append current to the end of the lessThan list and update lessThanEnd.
+            } else {
+                lessThanEnd->next = current;
+                lessThanEnd = current;
+            }
+            lessThanEnd->next = NULL;  // Set the next pointer of the last node to NULL
+        } else {
+            current->next = greaterThanOrEqual;
+            greaterThanOrEqual = current;
+        }
+        current = temp;
+
+    // Concatenate the two lists
+    if (lessThan == NULL) {
+        return greaterThanOrEqual;
+    } else {
+        lessThanEnd->next = greaterThanOrEqual;
+
+
 
 void display(NodeP* head)
 {
